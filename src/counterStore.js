9c2c3348d26f186bb8357e9738c1ad504f0fca28
Zain Fathoni <me@zainf.dev>
2023-07-06 21:56:33 +0800

    fix(counter): Fix counterStore.js to persist count in storage

--- src/counterStore.js
+++ src/counterStore.js
@@ -1,9 +1,17 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useCounterStore = create((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}));
+const useCounterStore = create(
+  persist(
+    (set, get) => ({
+      count: 0,
+      increment: () => set({ count: get().count + 1 }),
+      decrement: () => set({ count: get().count - 1 }),
+    }),
+    {
+      name: "counter-storage", // name of the item in the storage (must be unique)
+    },
+  ),
+);
 
 export default useCounterStore;
